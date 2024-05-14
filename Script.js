4ddada586e2ce5c55095e458ddae22ef787f8de3
Yesn't <131057815+YesTheProxy@users.noreply.github.com>
2024-05-13 20:34:43 -0400

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,7 +1,11 @@
 function createLag() {
   // Simulate heavy processing
-  for (let i = 0; i < 10000000000; i++) {
-    // Do some meaningless calculations
-    Math.sqrt(Math.random() * i);
+  const startTime = Date.now();
+  while (Date.now() - startTime < 5000) {
+    // Perform more complex calculations
+    let result = 0;
+    for (let i = 0; i < 100000; i++) {
+      result += Math.sqrt(Math.random() * i);
+    }
   }
 }
